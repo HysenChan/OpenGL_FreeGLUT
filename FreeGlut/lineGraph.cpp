fb7f09edeaa23efa043203074316f76f82c7e38d
HysenChan <HysenChan@163.com>
2021-02-22 11:11:58 +0800

    填充图

--- FreeGlut/lineGraph.cpp
+++ FreeGlut/lineGraph.cpp
@@ -52,6 +52,31 @@ void lineGraph(void)
 	glFlush();
 }
 
+void barChart(void)
+{
+	GLint month, k;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(1.0, 0.0, 0.0);
+	for (k = 0; k < 12; k++)
+	{
+		glRecti(20 + k * 50, 165, 40 + k * 50, dataValue[k]);
+	}
+	glColor3f(0.0, 0.0, 0.0);
+	xRaster = 20;
+	for (month = 0; month < 12; month++)
+	{
+		glRasterPos2i(xRaster, yRaster);
+		for (k = 3*month; k < 3*month+3; k++)
+		{
+			glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, label[k]);
+		}
+		xRaster += 50;
+	}
+	glFlush();
+}
+
 void winReshapeFcn(GLint newWidth, GLint newHeight)
 {
 	glMatrixMode(GL_PROJECTION);
@@ -70,7 +95,8 @@ void main(int argc, char** argv)
 	glutCreateWindow("Line Chart Data Plot");
 
 	init();
-	glutDisplayFunc(lineGraph);
+	//glutDisplayFunc(lineGraph);
+	glutDisplayFunc(barChart);
 	glutReshapeFunc(winReshapeFcn);
 
 	glutMainLoop();
